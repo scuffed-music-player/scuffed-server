3255bb8339db37142393bbbf1e12ed3f1f890747
Raghav Misra <raghav.m2014@gmail.com>
2021-10-31 00:08:36 -0700

    Yay route streams song from id!

---
+++ index.js
@@ -0,0 +1,20 @@
+const streamify = require("youtube-audio-stream");
+const express = require("express");
+
+const app = express();
+
+app.get("/song/:id", async (req, res) => {
+    try {
+        for await (const chunk of streamify(`http://youtube.com/watch?v=${req.params.id}`)) {
+            res.write(chunk);
+        }
+        res.end("Song finished!")
+    } catch (err) {
+        if (!res.headersSent) {
+            res.writeHead(500);
+            res.end("Something goofed! Maybe that song doesn't exist?");
+        }
+    }
+});
+
+app.listen(6969, () => console.log("Listening on 6969"));
