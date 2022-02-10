93b6ba5c381fe9c04aec72e1b70c1f0ed7d429d3
Raghav Misra <raghav.m2014@gmail.com>
2022-02-09 18:11:49 -0800

    Buff proxy console log

--- api/proxy.ts
+++ api/proxy.ts
@@ -2,6 +2,6 @@ import { Handler } from "express";
 import request from "request";
 
 export const useProxyRoute: () => Handler = () => async (req, res) => {
-    console.log(req.params);
+    console.log("Proxy request", req.params), ".";
     request.get(req.params.url).pipe(res);
 }
