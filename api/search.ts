cd9e60ed46101b335e91b1894b9963762333be55
Raghav Misra <raghav.m2014@gmail.com>
2022-02-10 22:11:39 -0800

    test search keywords for refined results

--- api/search.ts
+++ api/search.ts
@@ -3,14 +3,24 @@ import { youtube } from "scrape-youtube";
 import { ISongData } from "../typings";
 import { promises as fs } from "fs";
 
+const search = async (q: string) => {
+    let result = (await youtube.search(`${q} song`, {
+        type: "video"
+    })).videos[0];
+
+    if (result.title.toLowerCase().includes("official video") || result.title.toLowerCase().includes("music video")) {
+        result = (await youtube.search(`${q} audio`, {
+            type: "video"
+        })).videos[0];
+    }
+
+    return result;
+}
+
 export const useSearchRoute: () => Handler = () => async (req, res) => {
     try {
         const query = req.params.query.toLowerCase().trim();
-
-        const searchResults = await youtube.search(query, {
-            type: "video"
-        });
-        const target = searchResults.videos[0];
+        const target = await search(query);
 
         let downloaded: boolean = false;
 
