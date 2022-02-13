4d49fb3b8d0afe25a11521fa43b369aafc04a982
Raghav Misra <raghav.m2014@gmail.com>
2022-02-12 18:06:31 -0800

    simplify playlists route to get/set

--- api/playlists.ts
+++ api/playlists.ts
@@ -1,70 +1,47 @@
 import { Router } from "express";
 import { IPlaylist } from "../typings";
-import { v4 } from "uuid";
 import { promises as fs } from "fs";
 
-const playlists: IPlaylist[] = [];
-
-const generateId = (): string => {
-    const id = v4();
-    const exists = playlists.findIndex(p => p._id === id) > -1;
-    return exists ? generateId() : id;
-}
-
-const savePlaylists = () => fs.writeFile(`${process.cwd()}/data/playlists.json`, JSON.stringify(playlists));
-
-fs.readFile(`${process.cwd()}/data/playlists.json`, { encoding: "utf8" })
-    .then((data) => {
-        try { playlists.push(...JSON.parse(data)) }
-        catch (err) {}
-    })
-    .catch(savePlaylists);
+const setPlaylists = (playlists: IPlaylist[]) => fs.writeFile(
+    `${process.cwd()}/data/playlists.json`, 
+    JSON.stringify(playlists)
+);
+async function getPlaylists(): Promise<IPlaylist[]> {
+    try {
+        await fs.access(`${process.cwd()}/data/playlists.json`);
+        return JSON.parse(await fs.readFile(`${process.cwd()}/data/playlists.json`, "utf8"));
+    } catch (err) {
+        await fs.writeFile(`${process.cwd()}/data/playlists.json`, "[]");
+        return [];
+    }
+};
 
 const playlistRoutes = Router();
 
 // Get playlists:
-playlistRoutes.get("/", (req, res) => res.send({ 
-    success: true, playlists,
-}));
-
-// Update existing playlist
-playlistRoutes.post("/:id", async (req, res) => {
-    const newPlaylist: Partial<IPlaylist> = req.body;
-    const target = playlists.findIndex(p => p._id === req.params.id);
-    
-    if (target > -1) {
-        playlists[target] = {
-            _id: playlists[target]._id,
-            name: newPlaylist.name || playlists[target].name,
-            songs: newPlaylist.songs || playlists[target].songs,
-        };
-        
-        await savePlaylists();
-        
-        return res.status(201).json({
+playlistRoutes.get("/", async (req, res) => {
+    try {
+        res.status(200).json({
             success: true,
+            playlists: await getPlaylists(),
         });
+    } catch (error) {
+        res.status(500).json({ success: false, error });
     }
 });
 
-// Make a new playlist
+// Update existing playlist
 playlistRoutes.post("/", async (req, res) => {
-    const { name } = req.body;
+    try {
+        if (!Array.isArray(req.body.playlists)) {
+            throw new Error("Invalid request body.");
+        }
 
-    const newPlaylist: IPlaylist = {
-        _id: generateId(), 
-        name,
-        songs: [],
+        await setPlaylists(req.body.playlists);
+        res.status(201).json({ success: true, });
+    } catch (error) {
+        res.status(500).json({ success: false, error: (error as Error).message });
     }
-
-    playlists.push(newPlaylist);
-
-    await savePlaylists();
-
-    res.status(201).json({
-        success: true,
-        playlist: newPlaylist,
-    });
-})
+});
 
 export { playlistRoutes };
